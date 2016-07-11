1b61df6ba95b6c12b5cdd0b25232e6fe64dd088e
San Lin Naing <sanlinnaing2008@gmail.com>
2016-07-12 01:50:31 +0630

    remove old file

--- index.js
+++
@@ -1,8 +0,0 @@
-var data = require("sdk/self").data;
-var pageMod = require("sdk/page-mod");
-
-pageMod.PageMod({
-  include: "*",
-  contentStyleFile: data.url("zawgyi.css"),
-  contentScriptFile: [data.url("parabaik.js"),data.url("converter.js")]
-});
